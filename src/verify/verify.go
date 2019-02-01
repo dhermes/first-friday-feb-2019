22853f5a23abe98fcedf3debd1e946207693fed2
Danny Hermes <dhermes@blend.com>
2019-02-01 14:45:54 -0800

    Adding `verify` package (this took way too long).

--- src/verify/verify.go
+++ src/verify/verify.go
@@ -1,5 +1,31 @@
 package verify
 
-func Zoo() string {
-	return "a"
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func Parse(token string, publicKeyPEMBytes []byte) (*jwt.Token, error) {
+	derBlock, _ := pem.Decode(publicKeyPEMBytes)
+	if derBlock == nil {
+		return nil, errors.New("Failed to decode PEM block containing public key.")
+	}
+
+	publicKey, err := x509.ParsePKCS1PublicKey(derBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		return publicKey, nil
+	}
+
+	parsed, err := jwt.Parse(token, keyFunc)
+	if err != nil {
+		return nil, err
+	}
+	return parsed, nil
 }
--- src/verify/verify_test.go
+++ src/verify/verify_test.go
@@ -1,11 +1,39 @@
 package verify
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
 
-func TestZoo(t *testing.T) {
-	result := Zoo()
-	expected := "a"
-	if result != expected {
-		t.Errorf("got '%s' want '%s'", result, expected)
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func TestParse(t *testing.T) {
+	tokenBytes, err := ioutil.ReadFile("test_data/example_jwt.txt")
+	if err != nil {
+		t.Errorf("Failed to load example JWT: %v.", err)
+		return
+	}
+	token := string(tokenBytes) // Should check ASCII encoding.
+
+	publicKeyBytes, err := ioutil.ReadFile("test_data/id_rsa.pub.pem")
+	if err != nil {
+		t.Errorf("Failed to load example public key: %v.", err)
+		return
+	}
+
+	var parsed *jwt.Token
+	parsed, err = Parse(token, publicKeyBytes)
+	if err != nil {
+		t.Errorf("Failed to parse example JWT: %v.", err)
+		return
+	}
+	if parsed == nil {
+		t.Error("Parsed token is `nil`.")
+		return
+	}
+
+	if !parsed.Valid {
+		t.Errorf("JWT was invalid.")
+		return
 	}
 }
