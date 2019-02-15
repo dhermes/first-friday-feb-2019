8eb47aa22ac67c03a2b36fce9c4c38567da09c0d
Danny Hermes <dhermes@blend.com>
2019-02-15 09:29:22 -0800

    Stashing changes.
    This is a partial implementation of retrieving from Dynamo via `who-am-i`.

--- src/who-am-i/main.go
+++ src/who-am-i/main.go
@@ -8,11 +8,26 @@ import (
 	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 
 	"github.com/dhermes/first-friday-feb-2019/pkg/verify"
 )
+
+type KeyPackage struct {
+	KeyName string `json:"keyName"`
+	Key     string `json:"key"`
+}
+
+type DbItem struct {
+	UserId string     `json:"user_id"`
+	Key    KeyPackage `json:"key"`
+}
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
 // AWS Lambda Proxy Request functionality (default behavior)
@@ -24,12 +39,27 @@ type Response events.APIGatewayProxyResponse
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
 	var err error
 
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-1")},
+	)
+	svc := dynamodb.New(sess)
+	result, err := svc.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("auth-keys"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"user_id": {
+				S: aws.String("8f055c6f-7e8d-4cb0-b243-65c7d633df36"), // Hardcoded; very wrong
+			},
+		},
+	})
+	item := DbItem{}
+	_ = dynamodbattribute.UnmarshalMap(result.Item, &item)
+
 	authorization := request.Headers["Authorization"]
 	if !strings.HasPrefix(authorization, "Bearer ") {
 		return Response{StatusCode: 401}, errors.New("Unauthorized.")
 	}
 	bearerTokenJWT := authorization[7:]
-	publicKeyPEMBytes := []byte("") // TODO: Get these from somewhere.
+	publicKeyPEMBytes := []byte(item.Key.Key) // TODO: Get these from somewhere.
 	var valid bool
 	valid, err = verify.Verify(bearerTokenJWT, publicKeyPEMBytes, time.Now())
 	if err != nil || !valid {
